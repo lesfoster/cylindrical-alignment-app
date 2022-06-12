0c4671f0e4ebd54b4d4df506fb84195d97812a0f
lesfoster <lfoster.se.be@gmail.com>
2022-06-12 10:03:37 -0400

    Created a separate mouse-click listener so that users do not have to drag in order to select

---
+++ Viewer/src/self/lesfoster/cylindrical_alignment/viewer/java_fx/events/MouseClickedHandler.java
@@ -0,0 +1,56 @@
+/*
+ CDDL HEADER START
+
+ The contents of this file are subject to the terms of the
+ Common Development and Distribution License (the "License").
+ You may not use this file except in compliance with the License.
+
+ You can obtain a copy of the license at
+   https://opensource.org/licenses/CDDL-1.0.
+ See the License for the specific language governing permissions
+ and limitations under the License.
+
+ When distributing Covered Code, include this CDDL HEADER in each
+ file and include the License file at
+    https://opensource.org/licenses/CDDL-1.0.
+ If applicable, add the following below this CDDL HEADER, with the
+ fields enclosed by brackets "[]" replaced with your own identifying
+ information: Portions Copyright [yyyy] [name of copyright owner]
+
+ CDDL HEADER END
+*/
+
+
+package self.lesfoster.cylindrical_alignment.viewer.java_fx.events;
+
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.PickResult;
+import javafx.scene.shape.Shape3D;
+
+/**
+ * Handle mouse click by selecting picked node.
+ * @author Leslie L Foster
+ */
+public class MouseClickedHandler implements EventHandler<MouseEvent> {
+	private final GlyphSelector shapeSelector;
+			
+	public MouseClickedHandler(GlyphSelector glyphSelector) {
+		this.shapeSelector = glyphSelector;
+	}
+	
+	@Override
+	public void handle(MouseEvent me) {			
+		// Select the shape visually.
+		PickResult pr = me.getPickResult();
+		if (pr != null) {
+			Node node = pr.getIntersectedNode();
+			if (node != null && node instanceof Shape3D) {
+				shapeSelector.select(node);
+			}
+		}
+
+	}
+	
+}
